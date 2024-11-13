e33dcde1635f3dee2adaa0d99f71f8d333e20180
riyu.iring <oribelloruthiemy@gmail.com>
2024-11-13 23:05:59 +0800

    change design(not yet done) and make the website responsive

--- public/header.php
+++ public/header.php
@@ -1,5 +1,5 @@
 <header class="flex flex-col  fixed z-50 top-0 left-0 right-0  bg-[#FBF0F4] shadow-lg ">
-        <nav class="flex items-center justify-between px-[5%] py-6 bg-[#F05A28] w-full max-[640px]:px-[2%] ">
+        <nav class="flex items-center justify-between px-[5%] py-6 border border-b-[#d3d3d3] bg-white w-full max-[640px]:px-[2%] ">
             <div class="relative min-[1030px]:hidden"> 
                 <button id="burger_menuBttn" class="flex items-center "><img src="../assets/icons/menu-white.svg" alt=""></button>
                   <div id="burger_menu" class="hidden fixed top-[88px] bottom-0 left-0 right-0 ">
@@ -27,25 +27,25 @@
                  </div>
              </div>
             <div  id="logo" class="flex items-center cursor-pointer basis-1/5 max-[1030px]:basis-0">
-                        <img src="../assets/img/lmar_logo3.png" alt="" class=" w-[58px] object-contain drop-shadow-sm max-[640px]:w-[48px]">
-                     <p class="text-[22px] font-bold ml-[-7px] text-[#FCDC2A] drop-shadow-sm max-[640px]:text-[18px ">LMARHardware</p>
+                        <img src="../assets/img/lmar_logo_black_nobg.png" alt="" class=" w-[58px] object-contain max-[640px]:w-[48px]">
+                     <p class="text-2xl font-bold ml-[-7px] text-[#1d1d1d]  max-[640px]:text-[18px] ">LMARHardware</p>
                 </div>
                     <form action="" method="get" class="flex items-center justify-end justify-self-center basis-1/2 w-full  max-[1030px]:hidden ">
-                        <input type="text" name="" id="" placeholder="Search..." class=" w-[70%] px-[1.6rem] py-[0.5rem] text-[1rem] rounded-l  ">
+                        <input type="text" name="" id="" placeholder="Search..." class="  w-[70%] px-[1.6rem] py-[0.5rem] text-[1rem] rounded-l smShadow bg-white focus:outline-none focus:bg-slate-50 focus:shadow-none focus:border focus:border-[#d3d3d3]   ">
                         <button type="submit" value="" class="px-[1.6rem] py-[0.6rem] bg-[#FB951C] rounded-r ml-[-2px] ">
                             <img src="../assets/icons/magnifying-glass.png" alt="search icon by Taufik Glyph on flaticon" class="size-6 ">
                         </button>
                     </form>
                 <!-- <a href="products.php" class=" font-poppins text-[14px] font-medium  ">Products</a> -->
-                  
+
                       <div class="flex items-center justify-end gap-6 basis-1/4">
                             <div id="cart" class="relative flex items-end gap-2 cursor-pointer">
-                                <p class="text-xl font-semibold text-[#FCDC2A] max-[1030px]:hidden">Cart</p>
+                                <p class="text-xl font-bold text-[#1d1d1d] max-[1030px]:hidden">Cart</p>
                                 <img src="../assets/icons/tool-box (1).png" alt="" class="size-9 mr-2 max-[640px]:size-8">
                                 <div id="cart_items_count" class="absolute top-[-5px] right-0 bg-black/80 px-2 py-1 rounded-[50%]">
                                     <p class="text-white text-[10px]" >0</p>
                                 </div>
-                                <div id="cartContainer" class="fixed hidden flex-col  justify-center items-center top-[80px] right-[10%] min-w-[250px] max-w-[400px] h-[200px] bg-white rounded-lg py-4 px-3">
+                                <div id="cartContainer" class="fixed hidden flex-col  justify-center items-center top-[80px] right-[10%] min-w-[250px] max-w-[400px] h-[200px] bg-white border border-[#d3d3d3] rounded-lg py-4 px-3">
                                     <p>Empty cart... </p> 
                                     <!-- <div id="productCart" class="flex border-b-2 w-full h-fit  my-4 items-start justify-between">
                                         <img src="../assets/img/huhuh 1.png" alt="" class="size-11">
@@ -56,7 +56,7 @@
                                 </div>
                             </div>
                      
-                        <button id="loginBtn" class="flex gap-2 white-btn font-poppins px-4 py-2 text-[1rem] font-medium  max-[1030px]:hidden"> <img src="../assets/icons/circle-user-round (1).svg" alt=""> Log in</button>
+                        <button id="loginBtn" class=" rounded bg-[#1d1d1d] text-white  px-4 py-1 text-lg max-[1030px]:hidden">Log in</button>
                 </div>
         </nav>
         <nav class="bg-white px-52 py-5 flex items-center justify-center gap-5 max-[1030px]:hidden">
--- public/landing_page.php
+++ public/landing_page.php
@@ -10,80 +10,82 @@
     <link rel="stylesheet" href="./tailwind.css?v=<?php echo time(); ?>">
     <link rel="shortcut icon" href="../assets/img/lmar_logo.png" type="image/x-icon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/3.5.0/remixicon.css" crossorigin="">
-    <link rel="stylesheet" href="../css/swiper-bundle.min.css?v=<?php echo time(); ?>">
+      <link rel="stylesheet" href="./tailwind.css">
     <title>LMAR Hardware</title>
+    <style>
+        .drag-none{
+            -webkit-user-drag: none;
+            -khtml-user-drag: none;
+            -moz-user-drag: none;
+            -o-user-drag: none;
+            user-drag: none;
+        }
+        
+    </style>
 </head>
 <body>
   
     
     <form action="" method="get" class="hidden mt-[105px] items-center justify-center justify-self-center w-full py-3  max-[1030px]:flex ">
-        <input type="text" name="" id="" placeholder="Search..." class=" w-[70%] px-[1.6rem] py-[0.5rem] text-[1rem] rounded-l bg-white ">
+        <input type="text" name="" id="" placeholder="Search..." class=" w-[70%] px-[1.6rem] py-[0.5rem] text-[1rem] rounded-l smShadow bg-white focus:outline-none focus:bg-slate-50 focus:shadow-none focus:border focus:border-[#d3d3d3] ">
         <button type="submit" value="" class="px-[1.6rem] py-[0.6rem] bg-[#FB951C] rounded-r ml-[-2px] ">
             <img src="../assets/icons/magnifying-glass.png" alt="search icon by Taufik Glyph on flaticon" class="size-6 ">
         </button>
     </form>
-    <!-- max-w-[1450px] bg-[#F05A28]/10 -->
-    <section id="banner" class=" bg-[#F05A28] relative overflow-hidden w-full h-[100vh] mt-[90px] max-[500px]:h-[50vh]  max-[1030px]:h-[65vh]  max-[1030px]:mt-0">
-        <img src="../assets/img/emetn.png" alt="" class="absolute bottom-0 right-0 object-fill w-[50%] max-[800px]:-bottom-16 max-[800px]:-right-12  z-0 max-[500px]:w-[80%] max-[1030px]:w-[50%]   ">  
-        <!-- <div class="absolute w-full h-full top-0 right-0 bottom-0 overflow-hidden">
-            sm:w-full sm:h-[70vh] sm:bg-[#F05A28]/50 clip-banner
-            <div class="clip-banner absolute left-0 top-0 bottom-0 w-[75%] h-full z-10  max-[640px]:hidden min-[641px]:w-[90%] max-[1030px]:w-[90%]"></div>
-            <img src="../assets/img/emetn.png" alt="" class="absolute bottom-0 right-0 object-cover size-[150px] z-0 max-[640px]:w-full    ">  
-            <img src="../assets/img/banner.png" alt="" srcset="" class="w-full h-full object-cover">
-        </div> -->
-         
-        <div class="absolute top-0 bottom-0 left-[5%]  flex flex-col justify-center  items-start overflow-hidden z-20 max-[500px]:items-center  max-[500px]:justify-start max-[500px]:top-12">
-            <h1 class="text-[75px] text-white  font-poppins font-semibold max-w-[700px] max-[500px]:text-center max-[500px]:w-full max-[500px]:text-[38px] max-[1030px]:text-[48px] ">Welcome to <span class="text-[#FCDC2A] text-[80px] max-[500px]:text-[58px] max-[1030px]:text-[56px] ">LMAR</span> Hardware</h1>
-                <div class="flex items-start flex-col gap-8 max-[500px]:items-center max-[500px]:gap-5 ">
-                    <p class="font-outfit font-light text-[16px] text-start text-[#FBF0F4] max-w-[500px] drop-shadow-md  max-[500px]:w-[70%]  max-[500px]:text-center max-[500px]:text-[12px] max-[1030px]:text-[14px]   ">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</p>
-                    <div class="flex gap-2 items-center  ">
-                        <button id="order_nowBtn" class="yellow-btn  font-poppins px-[1.6rem] py-[0.6rem] text-[1rem] max-[500px]:text-sm ">Order now</button>
-                        <button id="view_prodBtn" class="flex items-center gap-2 white-btn  font-poppins px-[1.6rem] py-[0.6rem] text-[1rem] w-fit max-[500px]:text-sm ">View products
-                        <img src="../assets/icons/next (2).png" alt="arrow icon by  Ilham Fitrotul Hayat on flatIcon " class="max-[500px]:size-4" >
-                        </button>
-                    </div>
+
+    <section id="banner" class="flex 2xl:w-full lg:h-screen min-[375px]:w-fit   ">
+        <div class=" flex items-center lg:flex-nowrap md:flex-wrap justify-center w-full h-full mx-12 lg:flex-row  min-[375px]:flex-col   min-[375px]:pt-11 ">
+            <div class="flex flex-1 flex-col  gap-2 max-w-[950px] lg:items-start  min-[375px]:items-center ">
+                <p class="lg:text-start  min-[375px]:text-center text-wrap font-bold sm:text-8xl  min-[375px]:text-6xl text-[#1d1d1d] ">Welcome to <span class="text-[#FB951C]">LMAR</span> Hardware</p>
+                <p class="md:text-lg  min-[375px]:text-sm lg:text-start  min-[375px]:text-center text-wrap text-[#1d1d1d]">Browse and shop easily to your next door hardware.</p>
+                <div class="flex items-center gap-2 my-4">
+                    <button id="order_nowBtn" class=" text-white bg-[#fb951c] rounded py-2 px-4">Order now</button>
+                    <button id="view_prodBtn" class="text-white bg-[#1d1d1d] rounded py-2 px-4 gap-2 flex items-center">View products
+                        <img src="../assets/icons/nucleo_icons/arrow-right.svg" alt="" >
+                    </button>
                 </div>
+            </div>
+            <img src="../assets/img/emetn.png" class="object-contain  lg:size-[500px] min-[375px]:size-[450px] object-center">  
+        </div>
+    </section>
+
+    <section class="flex items-start justify-center gap-8 mx-auto w-full h-fit my-8 flex-wrap">
+        <div class="flex flex-col h-full  w-[300px] gap-2  px-6 py-4 bg-[#fb951c] rounded-md bxShadow">
+            <div class="flex items-center gap-1 border-b-2 border-[#1d1d1d]/70 pb-2 ">
+                        <img src="../assets/icons/nucleo_icons/time-clock-2.svg" id='icon' alt="dmitri13 Lineal on flaticon" class="">
+                        <p class="text-2xl  font-semibold ">Open hours</p>
+            </div>
+            <div class="flex flex-col items-start">
+                <p class="text-md font-medium"> Monday to Saturday</p>
+                <p class="text-md font-medium">8:00 am - 5:00 pm</p>
+            </div>
+        </div>
+        <div class="flex flex-col w-[300px] gap-2  px-6 py-4 bg-[#fb951c] rounded-md bxShadow">
+            <div class="flex items-center gap-1 border-b-2 border-[#1d1d1d] pb-2  ">
+                        <img  src="../assets/icons/nucleo_icons/position-marker-2.svg" id='icon' alt="dmitri13 Lineal on flaticon" class="size-5">
+                        <p class="text-2xl gap-2 font-semibold ">Location</p>
+            </div>
+            <div class="flex flex-col items-start">
+                <p class="text-md font-medium text-wrap">Luyahan Housing, Villa Hermosa
+                    Pasonanca, Zamboanga City</p>
+                <a class="text-md font-medium underline">Get direction</a>
+            </div>
+        </div>
+        <div class="flex flex-col h-full  w-[300px] gap-2 px-6 py-4 bg-[#fb951c] rounded-md bxShadow">
+            <div class="flex items-center gap-1 border-b-2 border-[#1d1d1d] pb-2 ">
+                        <img  src="../assets/icons/nucleo_icons/send-message-2.svg" id='icon' alt="dmitri13 Lineal on flaticon" class="size-5">
+                        <p class="text-2xl  font-semibold  ">Contact us</p>
+            </div>
+            <div class="flex flex-col items-start">
+                <p class="text-md font-medium">Orders & Enquiries</p>
+                <p class="text-md font-medium underline">+612912344567</p>
+            </div>
         </div>
        
     </section>
-
-    <!-- w-[1450px] -->
-    <main class=" flex flex-col items-center h-fit self-center w-full  mx-auto">
-        
-        <div class=" flex flex-row h-fit items-center justify-center w-full my-5 max-[640px]:flex-col max-[640px]:my-2">
-                <div class="afterBannerDiv ">
-                    <img src="../assets/icons/clock (1).png" id='icon' alt="dmitri13 Lineal on flaticon" class="iconImg">
-                    <div class="flex flex-col mt-[-4px]">
-                        <p class="text-[24px]  font-poppins max-[1030px]:text-[22px] "><strong>Open hours</strong></p>
-                        <p class="text-sm ml-1 max-[1030px]:text-xs">Monday to Saturday <br> 8:00 am - 5:00 pm</p>
-                    </div>
-                </div>
-            
-                <div class="afterBannerDiv  ">
-                    <img  src="../assets/icons/pin.png" id='icon' alt="" class="iconImg">
-                    <div class="flex flex-col mt-[-4px]">
-                        <p class="text-[24px]  font-poppins max-[1030px]:text-[22px]"><strong>Location</strong></p>
-                        <p class="text-sm  ml-1 max-[1030px]:text-xs">Luyahan Housing, Villa Hermosa <br>Pasonanca, Zamboanga City</p>
-                        <a href=" " class="flex items-center ml-1 ">
-                            <button class=" getDirection">Get direction <img src="../assets/icons/next (3).png" alt="" class=""></button>
-                    
-                        </a>
-                    </div>
-                </div>
-  
-                <div class="afterBannerDiv ">
-                    <img  src="../assets/icons/send.png" id='icon' alt="" class="iconImg">
-                    <div class="flex flex-col mt-[-4px]">
-                        <p class="text-[24px]  font-poppins max-[1030px]:text-[22px]"><strong>Contact us</strong></p>
-                        <p class="text-sm ml-1 max-[1030px]:text-xs">Orders & Enquiries  </p>
-                        
-                        <p class="text-sm ml-1 underline max-[1030px]:text-xs">+612912344567</p>
-                    </div>
-                </div>
-        </div>
-        <section class="flex flex-col items-center justify-start justify-self-center px-10 py-6 h-fit  max-[1030px]:px-3 ">
-            <p  class="text-center w-full text-[28px] font-medium max-[640px]:text-[24px] ">Shop by Categories</p>
-            <div class="flex items-center justify-center flex-wrap w-fit h-fit my-8 gap-4 self-center max-[640px]:gap-1 ">
+        <section class="flex flex-col items-center justify-start justify-self-center px-10 py-6 h-fit mx-[10vw] w-[80vw]  max-[1030px]:px-3 ">
+            <p  class="text-center w-full text-3xl font-bold ">Shop by Categories</p>
+            <div class="flex items-center justify-start flex-wrap h-fit w-full my-8 gap-1 mx-auto max-[640px]:gap-1 ">
                 <a href=""class="categoryCont "><img src="../assets/category img/10.png" alt="" class="categoryImg"><p class="absolute top-0 bottom-0 right-0 left-0">All Products</p></a>
                 <a href=""class=" categoryCont "><img src="../assets/category img/1.png" alt="" class="categoryImg "><p class="absolute top-0 bottom-0 right-0 left-0">Hand Tools</p></a>
                 <a href=""class="categoryCont "><img src="../assets/category img/2.png" alt="" class="categoryImg"><p class="absolute top-0 bottom-0 right-0 left-0">Measuring Tools</p></a>
@@ -96,114 +98,27 @@
                 <a href=""class="categoryCont "><img src="../assets/category img/9.png" alt="" class="categoryImg"><p class="absolute top-0 bottom-0 right-0 left-0">Building Materials</p></a>
             </div>
        </section>
-       
-     <section class=" productCont flex flex-col items-start gap-6 px-10 py-12 h-fit  max-[640px]:px-3">
-         <div class=" text-[28px] font-medium drop-shadow-sm  max-[640px]:text-[24px] " >Top products</div> 
-         <div class="swiper mySwiper w-full h-fit overflow-hidden">
-            <div class="swiper-wrapper">
-                    <div class="prodDetails swiper-slide w-fit">
-                        <div class="product ">
-                            <a class="text variant">Building Materials</a>
-                            <div class="Group1 " >
-                                <img class="placeholderImage"  src="../assets/product_img/11.png" />
-                                <div class="content " >
-                                    <a class="heading  font-poppins">Concrete Hollow Block</a>
-                                    <p class="price" >PHP 19.00</p>
-                                    <p class="sold" >1k sold</p>
-                                </div>
-                            </div>
-                        <button class=" orange-btn">Add to cart</button>
-                        </div>  
-                    </div> 
-                    <div class="prodDetails swiper-slide w-fit">
-                        <div class="product ">
-                             <a class="text variant">Building Materials</a>
-                            <div class="Group1 " >
-                                <img class="placeholderImage"  src="../assets/product_img/12.png" />
-                                <div class="content " >
-                                <a class="heading  font-poppins">Deformed Bar</a>
-                                <p class="price" >PHP 80.00 - 650.00</p>
-                                <p class="sold" >1k sold</p>
-                                </div>
-                            </div>
-                        <button class=" orange-btn">Add to cart</button>
-                        </div>  
-                    </div> 
-                    <div class="prodDetails swiper-slide w-fit">
-                        <div class="product ">
-                            <a class="text variant">Wood Materials</a>
-                            <div class="Group1 " >
-                                <img class="placeholderImage"  src="../assets/product_img/13.png" />
-                                <div class="content " >
-                                <a class="heading  font-poppins">Plywood</a>
-                                <p class="price" >PHP 417.00 - 1065.00</p>
-                                <p class="sold" >1k sold</p>
-                                </div>
-                            </div>
-                        <button class=" orange-btn">Add to cart</button>
-                        </div>  
+
+       <p class="text-3xl text-center w-full font-semibold my-6">Top products</p>
+    <section  id="scrollArea" class="flex flex-col items-start gap-1 mx-[10vw] w-[80vw] overflow-x-scroll  overflow-hidden  ">
+        <div   class="w-screen flex justify-start cursor-default select-none gap-2 py-2 px-2  ">
+            <div class="product smShadow flex flex-col gap-2  w-[200px] hover:bxShadow cursor-pointer">
+                <a class="text variant">Building Materials</a>
+                <div class="flex flex-col px-2 " >
+                    <img class="drag-none "  src="../assets/product_img/11.png" />
+                    <div class="flex flex-col items-start " >
+                        <a class="heading  font-poppins">Concrete Hollow Block</a>
+                        <p class="price" >PHP 19.00</p>
+                        <p class="sold" >1k sold</p>
                     </div>
-                    <div class="prodDetails swiper-slide w-fit">
-                        <div class="product ">
-                            <a class="text variant">Building Materials</a>
-                            <div class="Group1 " >
-                                <img class="placeholderImage"  src="../assets/product_img/14.png" />
-                                <div class="content " >
-                                    <a class="heading  font-poppins">Republic Cement</a>
-                                    <p class="price" >PHP 245.00</p>
-                                    <p class="sold" >1k sold</p>
-                                </div>
-                            </div>
-                        <button class=" orange-btn">Add to cart</button>
-                        </div>  
-                    </div> 
-                    <div class="prodDetails swiper-slide w-fit">
-                        <div class="product ">
-                            <a class="text variant">Building Materials</a>
-                            <div class="Group1 " >
-                                <img class="placeholderImage"  src="../assets/product_img/15.png" />
-                                <div class="content " >
-                                    <a class="heading  font-poppins">White Sand</a>
-                                    <p class="price" >PHP 190.00 - 2,000.00</p>
-                                    <p class="sold" >1k sold</p>
-                                </div>
-                            </div>
-                        <button class=" orange-btn">Add to cart</button>
-                    </div>  
-                 </div> 
-                    <div class="prodDetails swiper-slide w-fit">
-                        <div class="product ">
-                            <a class="text variant">Building Materials</a>
-                            <div class="Group1 " >
-                                <img class="placeholderImage"  src="../assets/product_img/14.png" />
-                                <div class="content " >
-                                    <a class="heading  font-poppins">Republic Cement</a>
-                                    <p class="price" >PHP 245.00</p>
-                                    <p class="sold" >1k sold</p>
-                                </div>
-                            </div>
-                        <button class=" orange-btn">Add to cart</button>
-                        </div>  
-                    </div> 
-                    <div class="prodDetails swiper-slide w-fit">
-                        <div class="product ">
-                            <a class="text variant">Building Materials</a>
-                            <div class="Group1 " >
-                                <img class="placeholderImage"  src="../assets/product_img/15.png" />
-                                <div class="content " >
-                                    <a class="heading  font-poppins">White Sand</a>
-                                    <p class="price" >PHP 190.00 - 2,000.00</p>
-                                    <p class="sold" >1k sold</p>
-                                </div>
-                            </div>
-                        <button class=" orange-btn">Add to cart</button>
-                    </div>  
-                </div> 
-            </div>
-          </div>
-         <button id="view_allBtn" class="button  text-[1rem] py-[8px] px-[28px] o-y-btn  shadow-sm rounded" >View all</button>
+                </div>
+            <button class=" orange-btn">Add to cart</button>
+          </div> 
+        </div>
     </section>
-    </main>
+    <div class="flex justify-end mx-[10vw] my-3">
+        <button id="view_allBtn" class="text-white bg-[#1d1d1d] rounded py-2 px-4 gap-2 flex items-center">View all</button>
+    </div>
 
     <footer class=" flex flex-col  justify-center items-center self-center w-full  max-w-[1450px] h-fit mb-10 mx-auto px-6 ">
         <div class="flex flex-col justify-center gap-[16px] border-b-2 border-black w-full h-[300px] px-2">
@@ -230,8 +145,10 @@
             </div>
         </div>
     </footer>
-    <script src="../script/swiper-bundle.min.js"></script>
     <script src="../script/carousel.js"></script>
     <script src="../script/landing_page.js"></script>
+   
+      
 </body>
 </html>
+
