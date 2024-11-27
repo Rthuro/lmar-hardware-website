9c1bee7d4f64fd33fc7d67d1356a4000a58cc9ee
riyu.iring <oribelloruthiemy@gmail.com>
2024-11-27 18:44:34 +0800

    Update products.php

--- frontend/products.php
+++ frontend/products.php
@@ -22,10 +22,9 @@
     <link rel="stylesheet" href="./tailwind.css?v=<?php echo time(); ?>">
     <link rel="shortcut icon" href="./assets/img/lmar_logo.png" type="image/x-icon">
 </head>
-<body >
-   
-
-    <div class="hidden gap-1 items-center justify-center justify-self-center w-full mt-[110px] max-[1380px]:flex min-[1030px]:mt-[180px] min-[1030px]:justify-start min-[1030px]:pl-[22%] ">
+<body>
+    
+    <div class="hidden gap-1 items-center justify-center justify-self-center w-full mt-[110px]  max-[1030px]:flex min-[1030px]:mt-[180px] min-[1030px]:justify-start min-[1030px]:pl-[22%] ">
         <div class="">
             <button id="filter_Bttn" class="flex items-center gap-1 bg-white px-3 py-2 rounded-sm border border-black "><img src="./assets/icons/filter (1).svg" alt="" class="size-5"><p class="font-medium">Filters</p></button>
             <form id="filter_mobileCont" action="" method="get" class="hidden flex-col fixed top-0 left-0 w-[50%] max-w-[355px] bottom-0 filter border-[1px] bg-white p-4 z-50 ">
@@ -57,124 +56,127 @@
             </form>
         </div>
       
-        <form action="" method="get" class="flex  items-center justify-center  w-[65%] min-[1030px]:hidden ">
-            <input type="text" name="" id="" placeholder="Search..." class=" w-full px-[1.6rem] py-[0.5rem] text-[1rem] rounded-l bg-white ">
+        <form action="" method="get" class="flex  items-center justify-center  w-[65%] min-[1030px]:hidden my-3 ">
+            <input type="text" name="" id="" placeholder="Search..." class=" w-full px-[1.6rem] py-[0.5rem] text-[1rem] rounded-l bg-white border border-[#d3d3d3] ">
             <button type="submit" value="" class="px-[1.6rem] py-[0.6rem] bg-[#FB951C] rounded-r ml-[-2px] ">
                 <img src="./assets/icons/magnifying-glass.png" alt="search icon by Taufik Glyph on flaticon" class="size-auto">
             </button>
         </form>
     </div>
 
-    <main class=" flex self-center justify-around w-full max-w-[1350px] h-fit mx-auto gap-4 mt-[200px] mb-[50px] max-[1380px]:mt-3 ">
-        <form action="" method="get" class="filter w-1/4 border-[1px] h-fit bg-white p-4   max-[1380px]:hidden ">
-            <p class="text-[28px] pb-2">Filters</p>
-            <label for="availability" class="block text-[18px] pl-1 pb-1 border-b-[1px]">Availability</label>
-            <div class="block pl-2 mt-2 mb-1 ">
-                <input type="radio" name="availability" id="inStock">
-                <label for="inStock">In stock</label>
-            </div>
-        
-            <div class="block pl-2" >
-                <input type="radio" name="availability" id="outOfStock">
-                <label for="outOfStock">Out of stock</label>
-            </div>
-            <label for="priceRange" class="block text-[18px] pl-1 my-2 ">Price </label>
-            <input type="range" name="priceRange" id="priceRange2" min="0" max="10000" step="10" class="w-full block ">
-            <div class="flex justify-between items-center mt-1">
-                <p id="minPrice2">PHP</p>
-                <p id="maxPrice">PHP 10,000</p>
-            </div>
-            <div class="flex flex-col justify-between mt-6 gap-2">
-                <input type="submit" value="Filter" name="filter" class="py-2  o-y-btn rounded-[2px] cursor-pointer ">
-                <input type="submit" value="Clear All" name="clearFilter" class="py-2  outline-o-btn cursor-pointer ">
+    <div  class=" flex  justify-center w-[1350px] h-fit  mb-[50px] mt-[200px]    ">
+        <div class=" flex justify-center mx-auto  w-full gap-4">
+            <form action="" method="get" class="filter w-[300px] border-[1px] h-fit bg-white p-4  max-[1030px]:hidden  min-[1031px]:flex   ">
+                        <p class="text-[28px] pb-2">Filters</p>
+                        <label for="availability" class="block text-[18px] pl-1 pb-1 border-b-[1px]">Availability</label>
+                        <div class="block pl-2 mt-2 mb-1 ">
+                            <input type="radio" name="availability" id="inStock">
+                            <label for="inStock">In stock</label>
+                        </div>
+                    
+                        <div class="block pl-2" >
+                            <input type="radio" name="availability" id="outOfStock">
+                            <label for="outOfStock">Out of stock</label>
+                        </div>
+                        <label for="priceRange" class="block text-[18px] pl-1 my-2 ">Price </label>
+                        <input type="range" name="priceRange" id="priceRange2" min="0" max="10000" step="10" class="w-full block ">
+                        <div class="flex justify-between items-center mt-1">
+                            <p id="minPrice2">PHP</p>
+                            <p id="maxPrice">PHP 10,000</p>
+                        </div>
+                        <div class="flex flex-col justify-between mt-6 gap-2">
+                            <input type="submit" value="Filter" name="filter" class="py-2  o-y-btn rounded-[2px] cursor-pointer ">
+                            <input type="submit" value="Clear All" name="clearFilter" class="py-2  outline-o-btn cursor-pointer ">
 
-            </div>
-        </form>
-        <section class="productGrid grid-flow-col-dense grid gap-2 w-fit max-[640px]:gap-1 ">
-            <div class="productPageContainer ">
-                <img class=""  src="./assets/product_img/11.png" />
-                <a class="variant">Building Materials</a> 
-                <div class="productPageDetails" >
-                    <a class="heading ">Concrete Hollow Block</a>
-                    <p class="price" >PHP 19.00</p>
-                    <p class="sold" >1k sold</p>
-                </div>
-            </div>
-            <div class="productPageContainer ">
-                <img class=""  src="./assets/product_img/11.png" />
-                <a class="variant">Building Materials</a> 
-                <div class="productPageDetails" >
-                    <a class="heading ">Concrete Hollow Block</a>
-                    <p class="price" >PHP 19.00</p>
-                    <p class="sold" >1k sold</p>
-                </div>
-            </div>
-            <div class="productPageContainer ">
-                <img class=""  src="./assets/product_img/11.png" />
-                <a class="variant">Building Materials</a> 
-                <div class="productPageDetails" >
-                    <a class="heading ">Concrete Hollow Block</a>
-                    <p class="price" >PHP 19.00</p>
-                    <p class="sold" >1k sold</p>
-                </div>
-            </div>
-            <div class="productPageContainer ">
-                <img class=""  src="./assets/product_img/11.png" />
-                <a class="variant">Building Materials</a> 
-                <div class="productPageDetails" >
-                    <a class="heading ">Concrete Hollow Block</a>
-                    <p class="price" >PHP 19.00</p>
-                    <p class="sold" >1k sold</p>
-                </div>
-            </div>
-                <div class="productPageContainer ">
-                    <img class=""  src="./assets/product_img/11.png" />
-                    <a class="variant">Building Materials</a> 
-                    <div class="productPageDetails" >
-                        <a class="heading font-poppins">Concrete Hollow Block</a>
-                        <p class="price" >PHP 19.00</p>
-                        <p class="sold" >1k sold</p>
-                    </div>
-                </div>
-                <div class="productPageContainer ">
-                    <img class=""  src="./assets/product_img/11.png" />
-                    <a class="variant">Building Materials</a> 
-                    <div class="productPageDetails" >
-                        <a class="heading ">Concrete Hollow Block</a>
-                        <p class="price" >PHP 19.00</p>
-                        <p class="sold" >1k sold</p>
-                    </div>
-                </div>
-                <div class="productPageContainer ">
-                    <img class=""  src="./assets/product_img/11.png" />
-                    <a class="variant">Building Materials</a> 
-                    <div class="productPageDetails" >
-                        <a class="heading ">Concrete Hollow Block</a>
-                        <p class="price" >PHP 19.00</p>
-                        <p class="sold" >1k sold</p>
-                    </div>
-                </div>
-                <div class="productPageContainer ">
-                    <img class=""  src="./assets/product_img/11.png" />
-                    <a class="variant">Building Materials</a> 
-                    <div class="productPageDetails" >
-                        <a class="heading ">Concrete Hollow Block</a>
-                        <p class="price" >PHP 19.00</p>
-                        <p class="sold" >1k sold</p>
-                    </div>
-                </div>
+                        </div>
+                    </form>
+            <section class="productGrid grid-flow-col-dense grid gap-2 w-fit max-[640px]:gap-1 ">
+                        <div class="productPageContainer ">
+                            <img class=""  src="./assets/product_img/11.png" />
+                            <a class="variant">Building Materials</a> 
+                            <div class="productPageDetails" >
+                                <a class="heading ">Concrete Hollow Block</a>
+                                <p class="price" >PHP 19.00</p>
+                                <p class="sold" >1k sold</p>
+                            </div>
+                        </div>
+                        <div class="productPageContainer ">
+                            <img class=""  src="./assets/product_img/11.png" />
+                            <a class="variant">Building Materials</a> 
+                            <div class="productPageDetails" >
+                                <a class="heading ">Concrete Hollow Block</a>
+                                <p class="price" >PHP 19.00</p>
+                                <p class="sold" >1k sold</p>
+                            </div>
+                        </div>
+                        <div class="productPageContainer ">
+                            <img class=""  src="./assets/product_img/11.png" />
+                            <a class="variant">Building Materials</a> 
+                            <div class="productPageDetails" >
+                                <a class="heading ">Concrete Hollow Block</a>
+                                <p class="price" >PHP 19.00</p>
+                                <p class="sold" >1k sold</p>
+                            </div>
+                        </div>
+                        <div class="productPageContainer ">
+                            <img class=""  src="./assets/product_img/11.png" />
+                            <a class="variant">Building Materials</a> 
+                            <div class="productPageDetails" >
+                                <a class="heading ">Concrete Hollow Block</a>
+                                <p class="price" >PHP 19.00</p>
+                                <p class="sold" >1k sold</p>
+                            </div>
+                        </div>
+                            <div class="productPageContainer ">
+                                <img class=""  src="./assets/product_img/11.png" />
+                                <a class="variant">Building Materials</a> 
+                                <div class="productPageDetails" >
+                                    <a class="heading font-poppins">Concrete Hollow Block</a>
+                                    <p class="price" >PHP 19.00</p>
+                                    <p class="sold" >1k sold</p>
+                                </div>
+                            </div>
+                            <div class="productPageContainer ">
+                                <img class=""  src="./assets/product_img/11.png" />
+                                <a class="variant">Building Materials</a> 
+                                <div class="productPageDetails" >
+                                    <a class="heading ">Concrete Hollow Block</a>
+                                    <p class="price" >PHP 19.00</p>
+                                    <p class="sold" >1k sold</p>
+                                </div>
+                            </div>
+                            <div class="productPageContainer ">
+                                <img class=""  src="./assets/product_img/11.png" />
+                                <a class="variant">Building Materials</a> 
+                                <div class="productPageDetails" >
+                                    <a class="heading ">Concrete Hollow Block</a>
+                                    <p class="price" >PHP 19.00</p>
+                                    <p class="sold" >1k sold</p>
+                                </div>
+                            </div>
+                            <div class="productPageContainer ">
+                                <img class=""  src="./assets/product_img/11.png" />
+                                <a class="variant">Building Materials</a> 
+                                <div class="productPageDetails" >
+                                    <a class="heading ">Concrete Hollow Block</a>
+                                    <p class="price" >PHP 19.00</p>
+                                    <p class="sold" >1k sold</p>
+                                </div>
+                            </div>
 
-                <div class="productPageContainer ">
-                    <img class=""  src="./assets/product_img/11.png" />
-                    <a class="variant">Building Materials</a> 
-                    <div class="productPageDetails" >
-                        <a class="heading ">Concrete Hollow Block</a>
-                        <p class="price" >PHP 19.00</p>
-                        <p class="sold" >1k sold</p>
-                    </div>
-                </div>
-        </section>
-    </main>
+                            <div class="productPageContainer ">
+                                <img class=""  src="./assets/product_img/11.png" />
+                                <a class="variant">Building Materials</a> 
+                                <div class="productPageDetails" >
+                                    <a class="heading ">Concrete Hollow Block</a>
+                                    <p class="price" >PHP 19.00</p>
+                                    <p class="sold" >1k sold</p>
+                                </div>
+                            </div>
+                    </section>
+        </div>
+       
+    </div>
 
     <footer class=" flex flex-col  justify-center items-center self-center w-full  max-w-[1450px] h-fit mb-10 mx-auto px-6 ">
         <div class="flex flex-col justify-center gap-[16px] border-b-2 border-black w-full h-[300px] px-2">
