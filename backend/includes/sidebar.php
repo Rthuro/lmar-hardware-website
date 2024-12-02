9f13dcf7b7c423c5f5a9a6fda6132b1c0bf69be0
riyu.iring <oribelloruthiemy@gmail.com>
2024-12-03 01:45:03 +0800

    Create sidebar.php

---
+++ backend/includes/sidebar.php
@@ -0,0 +1,13 @@
+
+<div class="sidebar">
+        <h2>LMAR Hardware</h2>
+        <ul>
+            <li><a class="dashboard"  href="/backend/dashboard/dashboard.php">Dashboard</a></li>
+            <li><a class="inventory" href="/backend/dashboard/inventory.php">Inventory</a></li>
+            <li><a class="orders" href="/backend/dashboard/orders.php">Orders</a></li>
+            <li><a class="delivery" href="/backend/dashboard/delivery.php">Deliveries</a></li>
+            <li><a class="pickup" href="/backend/dashboard/pickups.php">Pickups</a></li>
+            <li><a class="account" href="/backend/dashboard/account.php">Account</a></li>
+            <li><a  href="/backend/account/logout.php">Logout</a></li>
+        </ul>
+</div>
