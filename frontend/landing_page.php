6055dfda8463979542e56ae171af53a944dc1ba6
riyu.iring <oribelloruthiemy@gmail.com>
2024-12-03 01:47:19 +0800

    Update landing_page.php

--- frontend/landing_page.php
+++ frontend/landing_page.php
@@ -1,6 +1,14 @@
 <?php
-    include_once './includes/header.php';
 
+    session_start();
+
+     if(isset($_SESSION['account'])){
+        $username = $_SESSION['account']['username'];
+        $email = $_SESSION['account']['email'];
+     } 
+
+     include_once './includes/header.php';
+     
 ?>
     <form action="" method="get" class=" mt-8 mb-6 items-center justify-center justify-self-center w-full py-3 lg:hidden  xs:flex ">
         <input type="text" name="" id="" placeholder="Search..." class=" w-9/12 px-6 py-2 text-md rounded-l bg-white border focus:outline-gray-200 focus:bg-slate-50   ">
@@ -21,11 +29,11 @@
                     </button>
                 </div>
             </div>
-            <img src="./assets/img/emetn.png" class="object-contain  lg:size-[500px] xs:size-[350px] object-center">  
+            <img src="./assets/img/banner.png" class="object-contain  lg:size-[500px] xs:size-[350px] object-center">  
         </div>
     </section>
    
-    <section class="flex items-start justify-center gap-8 mx-auto w-full h-fit my-8 flex-wrap">
+    <section class="flex items-start justify-center gap-8 mx-auto max-w-[1050px] h-fit my-8 flex-wrap">
         <div class="flex flex-col h-full  basis-72 gap-2  px-6 py-4 bg-customOrange  rounded-md shadow-md">
             <div class="flex items-center gap-1 border-b-2 border-gray-800 pb-2 ">
                         <i data-lucide="clock"></i>
@@ -58,31 +66,30 @@
        
     </section>
 
-        <section class="flex flex-col items-center justify-center justify-self-center lg:px-10 py-6 h-fit mx-auto w-[80vw]  xs:px-3 ">
+        <section class="flex flex-col items-center justify-center justify-self-center py-6 h-fit mx-auto max-w-[1050px]  xs:px-3 ">
             <p  class="text-center w-full text-3xl font-bold ">Shop by Categories</p>
-            <div id="categoryContainer" class="flex items-center justify-center flex-wrap h-fit w-full my-8 gap-1 ">
+            <div id="categoryContainer" class="grid lg:grid-cols-5 lg:grid-rows-2 md:grid-cols-3 md:grid-rows-3 min-[320px]:grid-cols-2 min-[320px]:grid-rows-2 h-fit w-full mx-auto my-8 gap-1 ">
                 <!-- Category list goes here from landing_page.js -->
             </div>
        </section>
 
-       <p class="text-3xl text-center w-full font-semibold my-6">Top products</p>
-    <section  id="scrollArea" class="flex flex-col items-start gap-1 mx-[10vw] w-[80vw] overflow-x-scroll  overflow-hidden  ">
-        <div   class="w-screen flex justify-start cursor-default select-none gap-2 py-2 px-2  ">
+       <p class="text-3xl text-center mx-auto max-w-[1050px] font-semibold my-3">Top products</p>
+        <div class="flex items-start max-w-[1050px] mx-auto">
             <div class="product smShadow border-[1px] border-[#d3d3d3] flex flex-col gap-2  w-[200px] hover:bxShadow cursor-pointer">
-                <a class="text variant">Building Materials</a>
-                <div class="flex flex-col px-2 " >
-                    <img class="drag-none "  src="./assets/product_img/11.png" />
-                    <div class="flex flex-col items-start " >
-                        <a class="heading  font-poppins">Concrete Hollow Block</a>
-                        <p class="price" >PHP 19.00</p>
-                        <p class="sold" >1k sold</p>
+                    <a class="text variant">Building Materials</a>
+                    <div class="flex flex-col px-2 " >
+                        <img class="drag-none "  src="./assets/product_img/11.png" />
+                        <div class="flex flex-col items-start " >
+                            <a class="heading  font-poppins">Concrete Hollow Block</a>
+                            <p class="price" >PHP 19.00</p>
+                            <p class="sold" >1k sold</p>
+                        </div>
                     </div>
-                </div>
-            <button class=" orange-btn">Add to cart</button>
-          </div> 
+                <button class=" orange-btn">Add to cart</button>
+            </div> 
         </div>
-    </section>
-    <div class="flex justify-end mx-[10vw] my-3">
+            
+    <div class="flex justify-end max-w-[1050px] mx-auto my-3">
         <button id="view_allBtn" class="text-white bg-[#1d1d1d] rounded py-2 px-4 gap-2 flex items-center">View all</button>
     </div>
     <?php
