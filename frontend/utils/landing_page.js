7498bdff052b4e8940ada06ee2f5fa5756fa54fb
riyu.iring <oribelloruthiemy@gmail.com>
2024-12-03 01:47:36 +0800

    Update landing_page.js

--- frontend/utils/landing_page.js
+++ frontend/utils/landing_page.js
@@ -12,32 +12,6 @@ view_allBtn.addEventListener('click',function (){
     window.location.href = "products.php";
 })
 
-const scrollArea = document.getElementById('scrollArea');
-let isDown = false;
-let startX;
-let scrollLeft;
-
-scrollArea.addEventListener('mousedown', (e) => {
-  isDown = true;
-  startX = e.pageX - scrollArea.offsetLeft;
-  scrollLeft = scrollArea.scrollLeft;
-});
-
-scrollArea.addEventListener('mouseleave', () => {
-  isDown = false;
-});
-
-scrollArea.addEventListener('mouseup', () => {
-  isDown = false;
-});
-
-scrollArea.addEventListener('mousemove', (e) => {
-  if (!isDown) return;
-  e.preventDefault();
-  const x = e.pageX - scrollArea.offsetLeft;
-  const walk = (x - startX) * 2; 
-  scrollArea.scrollLeft = scrollLeft - walk;
-});
 
 const category = [
   {
