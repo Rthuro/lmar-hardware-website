c69739751d677beaea775d1fa704505a37e10e1b
riyu.iring <oribelloruthiemy@gmail.com>
2024-12-03 01:44:57 +0800

    Create header.php

---
+++ backend/includes/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Login</title>
+    <link rel="stylesheet" href="/frontend/tailwind.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="shortcut icon" href="/frontend/assets/img/lmar_logo.png" type="image/x-icon">
+    <style>
+    .err{
+        color:red;
+        background-color: rgb(255, 238, 238);
+        padding: 16px 12px;
+        text-align: center;
+    }
+    </style>
+</head>
