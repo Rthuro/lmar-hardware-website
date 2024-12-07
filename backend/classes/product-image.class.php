75c48c35d6c2b3098468a55d318e0ccc796780a9
riyu.iring <oribelloruthiemy@gmail.com>
2024-12-07 11:35:18 +0800

    Update product-image.class.php

--- backend/classes/product-image.class.php
+++ backend/classes/product-image.class.php
@@ -4,16 +4,24 @@ require_once 'product.class.php';
 
 class ProductImage extends Product{
     public $file_path = '';
-    public $image_role = '';
 
-    function addImage() {
-        $sql = "INSERT INTO product_image (product_id, file_path) VALUES (:product_id, :file_path;";
+    function addImage($prodId) {
+        $sql = "INSERT INTO product_image (product_id, img) VALUES (:product_id, :img);";
         $query = $this->db->connect()->prepare($sql);
 
-        $last_inserted_product = $this->db->connect()->lastInsertId();
-
-        $query->bindParam(':product_id', $last_inserted_product);
-        $query->bindParam(':file_path', $this->file_path);
+        $query->bindParam(':product_id',$prodId);
+        $query->bindParam(':img', $this->file_path);
         return $query->execute();
+    }
+
+    function fetchImage($id){
+        $sql = "SELECT * FROM product_image WHERE product_id=:id";
+        $query = $this->db->connect()->prepare($sql);
+        $query->bindParam(':id', $id);
+        $data = null;
+        if ($query->execute()) {
+            $data = $query->fetch();
+        }
+        return $data;
     }
 }
