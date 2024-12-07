f190aeba5231591253dce11b78b0b5d6198e9250
riyu.iring <oribelloruthiemy@gmail.com>
2024-12-07 11:34:50 +0800

    Update dashboard.php

--- backend/dashboard/dashboard.php
+++ backend/dashboard/dashboard.php
@@ -3,13 +3,6 @@
 
     require_once "../classes/product.class.php";
 
-    session_start();
-
-    if (!isset($_SESSION['admin_logged_in'])) {
-        header("Location: index.php");
-        exit();
-    }    
-
     include_once "../includes/header.php";
 
     $productObj = new Product();
