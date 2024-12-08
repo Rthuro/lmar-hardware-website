0dce62e7d6d4e55ecce8c5f53173c97e84a113f4
riyu.iring <oribelloruthiemy@gmail.com>
2024-12-09 00:45:20 +0800

    Update product.class.php

--- backend/classes/product.class.php
+++ backend/classes/product.class.php
@@ -4,14 +4,10 @@ require_once 'database.php';
 
 class Product {
     public $id = '';
-    public $product_code = '';
     public $product_name = '';
+    public $product_img = '';
     public $category = '';
-    public $size = '';
-    public $price = '';
-    public $stocks = '';
-    public $sizePrice = '';
-
+    public $description = '';
     protected $db;
 
     function __construct() {
@@ -19,20 +15,35 @@ class Product {
     }
 
     function add() {
-        $sql = "INSERT INTO products (product_code, product_name, category, price, stocks) VALUES (:product_code, :product_name, :category, :price, :stocks);";
+        $sql = "INSERT INTO products (product_name, product_img, category, description) VALUES (:product_name, :product_img, :category, :description);";
         $query = $this->db->connect()->prepare($sql);
-        $query->bindParam(':product_code', $this->product_code);
         $query->bindParam(':product_name', $this->product_name);
+        $query->bindParam(':product_img', $this->product_img);
         $query->bindParam(':category', $this->category);
-        $query->bindParam(':price', $this->price);
-        $query->bindParam(':stocks', $this->stocks);
+        $query->bindParam(':description', $this->description);
         return $query->execute();
     }
 
+    function update(){
+        $sql = "UPDATE products SET product_name = :product_name, category = :category, description = :description ;";
+        $query = $this->db->connect()->prepare($sql);
+        $query->bindParam(':product_name', $this->product_name);
+        $query->bindParam(':category', $this->category);
+        $query->bindParam(':description', $this->description);
+        return $query->execute();
+
+    }
+    function delete(){
+        $sql = "DELETE FROM products WHERE id = :id";
+        $query = $this->db->connect()->prepare($sql);
+        $query->bindParam(':id', $this->id);
+        return $query->execute();
+        
+    }
+
     function showAll($keyword='', $category='') {
-        $sql = "SELECT i.*, p.*, c.name as category_name, SUM(IF(s.change_type='addition', quantity, 0)) as stock_in, SUM(IF(s.change_type='subtraction', quantity, 0)) as stock_out FROM products p 
-        INNER JOIN categories c ON p.category = c.id 
-        LEFT JOIN product_image i ON p.id = i.product_id LEFT JOIN stock_transactions s ON p.id = s.product_id WHERE (p.product_code LIKE CONCAT('%', :keyword, '%') OR p.product_name LIKE CONCAT('%', :keyword, '%')) AND (c.name LIKE CONCAT('%', :category, '%')) GROUP BY p.id ORDER BY p.product_name ASC;";
+        $sql = "SELECT  p.*, c.name as category_name, SUM(IF(s.change_type='addition', quantity, 0)) as stock_in, SUM(IF(s.change_type='subtraction', quantity, 0)) as stock_out FROM products p INNER JOIN categories c ON p.category = c.id 
+        LEFT JOIN stock_transactions s ON p.id = s.product_id WHERE p.product_name LIKE CONCAT('%', :keyword, '%') AND (c.name LIKE CONCAT('%', :category, '%')) GROUP BY p.id ORDER BY p.product_name ASC;";
 
         $query = $this->db->connect()->prepare($sql);
         $query->bindParam(':keyword', $keyword);
@@ -44,32 +55,18 @@ class Product {
         return $data;
     }
 
-    function edit() {
-        $sql = "UPDATE products SET product_code = :code, product_name = :name, category = :category_id, price = :price, stocks = :stocks WHERE id = :id;";
+    function checkProductDup(){
+        $sql = "SELECT * FROM products WHERE product_name = :product_name;";
         $query = $this->db->connect()->prepare($sql);
-        $query->bindParam(':code', $this->product_code);
-        $query->bindParam(':name', $this->product_name);
-        $query->bindParam(':category_id', $this->category);
-        $query->bindParam(':price', $this->price);
-        $query->bindParam(':stocks', $this->stocks);
-        $query->bindParam(':id', $this->id);
-        return $query->execute();
-    }
-
-    function checkForEdit($code, $id){
-        $sql = "SELECT COUNT(*) FROM products WHERE id = :id AND product_code = :code ;";
-        $query = $this->db->connect()->prepare($sql);
-        $query->bindParam(':id', $id);
-        $query->bindParam(':code', $code);
-
+        $query->bindParam(':product_name', $this->product_name);
+        $data = null;
         if ($query->execute()) {
-            $data = $query->fetch(PDO::FETCH_ASSOC);
-            return $data !== false;
+            $data = $query->fetchAll();
         }
-        return false;  
+        return $data;
     }
 
-    function fetchRecord($id) {
+     function fetchRecord($id) {
         $sql = "SELECT * FROM products WHERE id = :id;";
         $query = $this->db->connect()->prepare($sql);
         $query->bindParam(':id', $id);
@@ -79,31 +76,7 @@ class Product {
         }
         return $data;
     }
-
-    function delete($recordID) {
-        $sql = "DELETE FROM products WHERE id = :recordID;";
-        $query = $this->db->connect()->prepare($sql);
-        $query->bindParam(':recordID', $recordID);
-        return $query->execute();
-    }
-
-    function codeExists($code, $excludeID = null) {
-        $sql = "SELECT COUNT(*) FROM products WHERE product_code = :code";
-        if ($excludeID) {
-            $sql .= " AND id != :excludeID";
-        }
-        $query = $this->db->connect()->prepare($sql);
-        $query->bindParam(':code', $code);
-        if ($excludeID) {
-            $query->bindParam(':excludeID', $excludeID);
-        }
-        $query->execute();
-        $count = $query->fetchColumn();
-        return $count > 0;
-    }
-    
-
-     function fetchCategory() {
+    function fetchCategory() {
         $sql = "SELECT * FROM categories ORDER BY name ASC;";
         $query = $this->db->connect()->prepare($sql);
         $data = null;
@@ -113,19 +86,8 @@ class Product {
         return $data;
     }
 
-    function fetchCategoryName($id) {
-        $sql = "SELECT name FROM categories WHERE id = :id;";
-        $query = $this->db->connect()->prepare($sql);
-        $query->bindParam(':id', $id);
-        $data = null;
-        if ($query->execute()) {
-            $data = $query->fetch();
-        }
-        return $data;
-    }
-
-    function fetchRecentOrders(){
-        $sql = "SELECT orders.*, products.product_name FROM orders LEFT JOIN products ON orders.product_id = products.id ORDER BY orders.order_date DESC LIMIT 5;";
+     function displayTopProd(){
+        $sql = "SELECT p.*, c.name as category_name FROM products p  INNER JOIN categories c ON p.category = c.id LIMIT 4;";
         $query = $this->db->connect()->prepare($sql);
         $data = null;
         if ($query->execute()) {
@@ -134,102 +96,26 @@ class Product {
         return $data;
     }
 
-    function displayTopProd(){
-        $sql = "SELECT p.*, c.name as category_name, pi.img FROM products p  INNER JOIN categories c ON p.category = c.id JOIN product_image pi ON p.id = pi.product_id LIMIT 4;";
+    function getProducts($start, $limit) {
+        $sql = "SELECT p.*,c.name as category_name FROM products p INNER JOIN categories c ON p.category = c.id  LIMIT :start, :limit";
         $query = $this->db->connect()->prepare($sql);
-        $data = null;
-        if ($query->execute()) {
-            $data = $query->fetchAll(PDO::FETCH_ASSOC);
-        }
-        return $data;
+        $query->bindParam(':start', $start, PDO::PARAM_INT);
+        $query->bindParam(':limit', $limit, PDO::PARAM_INT);
+        $query->execute();
+        return $query->fetchAll(PDO::FETCH_ASSOC);
     }
-
-    function getLastInsertedId(){
-        $sql = "SELECT LAST_INSERT_ID() as id ;";
-        $query = $this->db->connect()->prepare($sql);
-        if ($query->execute()) {
-            $data = $query->fetch();
-        }
-        return $data;
-    }
-
-    function fetchProdNames(){
-        $sql = "SELECT * FROM products;";
-        $query = $this->db->connect()->prepare($sql);
-        $data = null;
-        if ($query->execute()) {
-            $data = $query->fetchAll(PDO::FETCH_ASSOC);
-        }
-        return $data;
-    }
-
-    function addProductSize($prodId){
-        $sql = "INSERT INTO product_size (product_id, size, price) VALUES (:product_id, :size, :price);";
-        $query = $this->db->connect()->prepare($sql);
-        $query->bindParam(':product_id', $prodId);
-        $query->bindParam(':size', $this->size);
-        $query->bindParam(':price', $this->sizePrice);
-        return $query->execute();
-    }
-
-    function deleteSize($sizeId) {
-        $sql = "DELETE FROM product_size WHERE id = :id;";
-        $query = $this->db->connect()->prepare($sql);
-        $query->bindParam(':id', $sizeId);
-        return $query->execute();
-    }
-
-    function checkSizeDup($prodId){
-        $sql = "SELECT * FROM product_size WHERE product_id = :product_id AND size = :size;";
-        $query = $this->db->connect()->prepare($sql);
-        $data = null;
-        $query->bindParam(':product_id', $prodId);
-        $query->bindParam(':size', $this->size);
-        if ($query->execute()) {
-            $data = $query->fetchAll(PDO::FETCH_ASSOC);
-        }
-        return $data;
-    }
-
-    function getSizesByProductId($prodId) {
-        $sql = "SELECT * FROM product_size WHERE product_id = :product_id;";
-        $query = $this->db->connect()->prepare($sql);
-        $query->bindParam(':product_id', $prodId);
-        $data = null;
-
-        if ($query->execute()) {
-            $data = $query->fetchAll(PDO::FETCH_ASSOC);
-        }
-        return $data;
-    }
-
-    function getSizesBySizeId($sizeId) {
-        $sql = "SELECT * FROM product_size WHERE id = :id;";
-        $query = $this->db->connect()->prepare($sql);
-        $query->bindParam(':id', $sizeId);
-        $data = null;
-        
-        if ($query->execute()) {
-            $data = $query->fetchAll(PDO::FETCH_ASSOC);
-        }
-        return $data;
-    }
-
-    function forProdPage() {
-        $sql = "SELECT COUNT(*) AS total FROM products WHERE 1=1"; // Add conditions dynamically if needed
-        $query = $this->db->connect()->prepare($sql);
     
-        if ($query->execute()) {
-            return $query->fetchColumn(); // Return the total count directly
-        }
-        return 0; // Default if no products found
+    
+    function getTotalProducts() {
+        $sql = "SELECT COUNT(*) AS total FROM products";
+        $query = $this->db->connect()->prepare($sql);
+        $query->execute();
+        return $query->fetchColumn();
     }
 
-    function modifyProdStock(){
-        $sql = "UPDATE products SET stocks = :stocks WHERE id = :id";
-        $query = $this->db->connect()->prepare($sql);
-        $query->bindParam(':stocks', $this->stocks);
-        $query->bindParam(':id', $this->id);
-        return $query->execute();
-    }
+ 
+
+    
+
+    
 }
